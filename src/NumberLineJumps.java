e74b89b2bcf2ce9a92c7b740b1c14432ddefddf7
Alex <alexeluro@gmail.com>
2020-11-16 14:48:14 +0100

    Initial commit

---
+++ src/NumberLineJumps.java
@@ -0,0 +1,15 @@
+public class NumberLineJumps {
+
+    public static void main(String[] args) {
+
+    }
+
+
+
+    // Complete the kangaroo function below.
+    static String kangaroo(int x1, int v1, int x2, int v2) {
+
+
+    }
+
+}
